157a17a874ddcd7a7bd8c4465caaa14049feb36e
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-09-17 19:48:11 +0200

    Se crea el componente ItemListContainer y se le pasa la prop greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './index.css'
 import NavBar from './components/Navbar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 function App() {
 
   return (
     <>
       <NavBar />
+      <ItemListContainer greeting="Bienvenid@ a la tienda online" />
     </>
   )
 }
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,23 @@
+
+const ItemListContainer = ({greeting}) => {
+  return (
+    <>
+        <main>
+            <div className="header-sup">
+                <div className="bienvenida text-center my-5">
+                    <div className="bienve">
+                        <h1>HERBORISTERÍA NATURAL</h1>
+                            <div className="usuario"> {/* llamada a usuario */}
+                            <p>{greeting} <span id="nombre-usuario"></span></p>
+                        </div>
+                        <span id="nombre-usuario" className="name"></span>
+                    </div>
+                    <br />
+                </div>
+            </div>
+        </main>
+    </>
+  )
+}
+
+export default ItemListContainer
