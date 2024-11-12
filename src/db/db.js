91a2214abd4ed9710b75c70ad1da3fcd0368d205
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-11-12 09:34:10 +0100

    Se aplican variables de entorno a la configuración de firebase en el archivo db.js

--- src/db/db.js
+++ src/db/db.js
@@ -3,11 +3,11 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "ecommerce-ch-react-pf.firebaseapp.com",
-  projectId: "ecommerce-ch-react-pf",
-  storageBucket: "ecommerce-ch-react-pf.appspot.com",
-  messagingSenderId: "597084827519",
-  appId: "1:597084827519:web:954d4a2e7f84c9dacbf44a"
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
